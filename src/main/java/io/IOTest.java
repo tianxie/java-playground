e2cb478a11920989743235d565dd1afe73fd8299
Tian Xie <xietian@ehousechina.com>
2017-07-11 14:08:14 +0800

    DigestInputStream and NIO tests

--- src/main/java/io/IOTest.java
+++ src/main/java/io/IOTest.java
@@ -1,18 +1,18 @@
 package io;
 
+import java.io.File;
+
 /**
  * Created by Tian on 2016/12/15.
  */
 public class IOTest {
     public static void main(String[] args) {
-//        File homedir = new File(System.getProperty("user.home"));
-//        File f = new File(homedir, "app.conf");
-//        if (f.exists() && f.isFile() && f.canRead()) {
-//            File configdir = new File(f, ".configdir");
-//            configdir.mkdir();
-//            f.renameTo(new File(configdir, ".config"));
-//        }
-
-        System.out.println(Math.sin(Math.toDegrees(45)));
+        File homedir = new File(System.getProperty("user.home"));
+        File f = new File(homedir, "app.conf");
+        if (f.exists() && f.isFile() && f.canRead()) {
+            File configdir = new File(f, ".configdir");
+            configdir.mkdir();
+            f.renameTo(new File(configdir, ".config"));
+        }
     }
 }
