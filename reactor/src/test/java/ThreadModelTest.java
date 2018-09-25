8ea839385f3d5656e78938c2d44e752adbd3f01d
Tian Xie <xietian@ehousechina.com>
2018-09-25 11:26:38 +0800

    reactor

---
+++ reactor/src/test/java/ThreadModelTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class ThreadModelTest {
+
+    @Test
+    public void test() {
+        Flux.interval(Duration.ofMillis(100))
+                .log()
+                .take(20)
+                .subscribe(i -> System.out.println(Thread.currentThread().getName()));
+    }
+}
