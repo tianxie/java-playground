864be3bb374a355c40c3dcf15306cc69507dd015
Tian Xie <xietian@outlook.com>
2019-12-13 13:10:35 +0800

    kafka-quickstart

---
+++ kafka-quickstart/src/main/java/me/txie/kafka/KafkaQuickstartApplication.java
@@ -0,0 +1,11 @@
+package me.txie.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaQuickstartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaQuickstartApplication.class, args);
+    }
+}
