21e2da760bf96a4ae8e0aca6e99958f0194cd86a
xietian <txie@live.cn>
2021-05-14 13:50:01 +0800

    spring-boot-kafka-testing

---
+++ spring-boot-kafka-testing/src/test/java/me/txie/springbootkafkatesting/SpringBootKafkaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.txie.springbootkafkatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
